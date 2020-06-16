66c74cb6e487c20f5ed0f3558ac2b1f187197d79
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-16 03:23:15 -0500

    Returned a list of all customers that live in London, Madrid, or Brazil

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT suppliername, phone from suppliers where supplierid = 11;
 -- List first 10 orders placed, sorted descending by the order date
 SELECT orderdate from orders order by orderdate desc;
 -- Find all customers that live in London, Madrid, or Brazil
-
+SELECT customername, country from customers where city = "Madrid"  or city = "London" or country = "Brazil";
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
