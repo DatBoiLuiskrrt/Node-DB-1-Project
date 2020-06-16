a646e3e84d8da25ec406e49000a056d2c5e4e068
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-06-16 02:57:46 -0500

    Added a get method that returns a list of accounts

--- api/server.js
+++ api/server.js
@@ -4,6 +4,20 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
+
 server.use(express.json());
+
+server.get('/', (req,res) => {
+
+    db.select('*').from('accounts')
+    .then( rows => {
+        res.status(200).json( { data: rows});
+    })
+    .catch( error => {
+        res.status(500).json( { message: 'Sorry ran into an error homes :('});
+    });
+});
+
+
 
 module.exports = server;
